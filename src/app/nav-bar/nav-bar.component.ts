66bb99fc95cf8ac0be1f9b44064401b1ee3027c9
WendyNgiye <ngiyewendy2@gmail.com>
2024-11-18 15:13:30 +0300

    menu

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  isMenuOpen = false; // State for menu visibility
+  screenWidth!: number;
 
+  ngOnInit() {
+    this.screenWidth = window.innerWidth; // Initial screen width
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth > 768) {
+      this.isMenuOpen = false; // Close menu when resizing to larger screens
+    }
+  }
+  
 }
